abb5f9dcc2d12e78a1502777778fcca5c81034ff
Ihfandi <39288119+ihfandica@users.noreply.github.com>
2026-09-07 14:11:59 +0700

    Update 6_CpData2DtlFktr.py

--- Dapur/6_CpData2DtlFktr.py
+++ Dapur/6_CpData2DtlFktr.py
@@ -21,6 +21,7 @@ def main():
     print("--> Sedang memuat file Helper...")
     helpers_kode = {
     	'000000': load_helper_list('Helper_000000.txt'),
+        '120101': load_helper_list('Helper_120101.txt'),
         '160100': load_helper_list('Helper_160100.txt'),
     	'200104': load_helper_list('Helper_200104.txt'),
         '340300': load_helper_list('Helper_340300.txt'),
